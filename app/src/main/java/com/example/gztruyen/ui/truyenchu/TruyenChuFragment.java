f6a9714e57c8589311d552bec2ada20d2b504364
oxit Nhôm <luutiendat12072001@gmail.com>
2023-03-07 17:11:55 +0700

    PRM-4 Creat Activity Login and Register

--- app/src/main/java/com/example/gztruyen/ui/truyenchu/TruyenChuFragment.java
+++ app/src/main/java/com/example/gztruyen/ui/truyenchu/TruyenChuFragment.java
@@ -24,6 +24,7 @@ import com.example.gztruyen.Activity.SearchActivity;
 import com.example.gztruyen.R;
 import com.example.gztruyen.adapters.TruyenChuAdapter;
 import com.example.gztruyen.adapters.TruyenTranhAdapter;
+import com.example.gztruyen.api.FireStoreApi;
 import com.example.gztruyen.databinding.FragmentTruyenChuBinding;
 import com.example.gztruyen.databinding.FragmentTruyenTranhBinding;
 import com.example.gztruyen.model.ComicModel;
@@ -57,6 +58,8 @@ public class TruyenChuFragment extends Fragment {
         binding = FragmentTruyenChuBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         context = root.getContext();
+        adapter = new TruyenChuAdapter();
+        adapter.updateData(getTruyen(adapter));
         gridLayoutManager = new GridLayoutManager(context,3);
         bindingView(root);
         bindingAction();
@@ -81,7 +84,6 @@ public class TruyenChuFragment extends Fragment {
         imageSlider.setItemClickListener(this::itemClicker);
         item_truyen.setLayoutManager(gridLayoutManager);
         item_truyen.setAdapter(adapter);
-        adapter = new TruyenChuAdapter(getTruyen());
     }
     private void itemClicker(int i) {
         Log.d( "Items","Clicked" + i);
@@ -97,8 +99,9 @@ public class TruyenChuFragment extends Fragment {
         }
         return imageList;
     }
-    private List<ComicModel> getTruyen() {
-        return null;
+    private List<ComicModel> getTruyen(TruyenChuAdapter adapter) {
+        List<ComicModel> list = FireStoreApi.getAllStory(adapter);
+        return list;
     }
     private void btnSearchOnClick(View view) {
 //        Toast.makeText(context, "As u wish", Toast.LENGTH_SHORT).show();
