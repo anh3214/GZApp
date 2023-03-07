49fadaa35a141df0a8847cfe98c6ed378f1f0f98
SuperQuangTeo <superteo0712@gmail.com>
2023-03-08 01:39:49 +0700

    get chapter api

---
+++ app/src/main/java/com/example/gztruyen/Activity/userDetailActivity/FollowFragment.java
@@ -0,0 +1,33 @@
+package com.example.gztruyen.Activity.userDetailActivity;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.gztruyen.R;
+
+
+public class FollowFragment extends Fragment {
+
+
+    public FollowFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_follow, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/gztruyen/model/DocumentChap.java
@@ -0,0 +1,42 @@
+package com.example.gztruyen.model;
+
+public class DocumentChap {
+    private String name;
+    private Fields fields;
+    private String createTime;
+    private String updateTime;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Fields getFields() {
+        return fields;
+    }
+
+    public void setFields(Fields fields) {
+        this.fields = fields;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }
+
+
+}
